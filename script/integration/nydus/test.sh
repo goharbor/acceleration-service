8c3c3cf93f3e0631485ccd08386a2e4f2a823e4a
Yadong Ding <ding_yadong@foxmail.com>
2023-10-10 09:30:04 +0800

    fix: remove useless args backend-type etc.
    ref: dragonflyoss/image-service#970
    
    Signed-off-by: Yadong Ding <ding_yadong@foxmail.com>

--- script/integration/nydus/test.sh
+++ script/integration/nydus/test.sh
@@ -13,12 +13,8 @@ sudo ./accelctl task create --sync localhost/library/$OCI_IMAGE_NAME:latest
 
 # Verify filesystem consistency for converted image
 sudo /usr/bin/nydusify check \
-  --nydus-image /usr/bin/nydus-image \
-  --nydusd /usr/bin/nydusd \
   --source localhost/library/$OCI_IMAGE_NAME:latest \
-  --target localhost/library/$OCI_IMAGE_NAME:latest-nydus \
-  --backend-type registry \
-  --backend-config "{\"scheme\":\"http\",\"host\":\"localhost\",\"repo\":\"library/$OCI_IMAGE_NAME\",\"auth\":\"YWRtaW46SGFyYm9yMTIzNDU=\"}"
+  --target localhost/library/$OCI_IMAGE_NAME:latest-nydus
 
 # Gracefully exit acceld
 sudo pkill -SIGINT acceld
--- script/integration/one-time.sh
+++ script/integration/one-time.sh
@@ -9,9 +9,5 @@ sudo ./accelctl convert --config ./misc/config/config.nydus.yaml localhost/libra
 
 # Verify filesystem consistency for converted image
 sudo /usr/bin/nydusify check \
-  --nydus-image /usr/bin/nydus-image \
-  --nydusd /usr/bin/nydusd \
   --source localhost/library/$OCI_IMAGE_NAME:latest \
-  --target localhost/library/$OCI_IMAGE_NAME:latest-nydus \
-  --backend-type registry \
-  --backend-config "{\"scheme\":\"http\",\"host\":\"localhost\",\"repo\":\"library/$OCI_IMAGE_NAME\",\"auth\":\"YWRtaW46SGFyYm9yMTIzNDU=\"}"
+  --target localhost/library/$OCI_IMAGE_NAME:latest-nydus
