41f7d7b06cca9ef2e247e889e069cd40bbb28472
abushwang <abushwangs@gmail.com>
2025-02-28 10:10:00 +0800

    Support zstdchunked conversion
    Signed-off-by: abushwang <abushwangs@gmail.com>

--- pkg/driver/estargz/estargz.go
+++ pkg/driver/estargz/estargz.go
@@ -16,16 +16,34 @@ package estargz
 
 import (
 	"context"
+	"fmt"
 	"strconv"
 
 	"github.com/containerd/containerd/images/converter"
 	"github.com/containerd/platforms"
 	"github.com/containerd/stargz-snapshotter/estargz"
 	estargzconvert "github.com/containerd/stargz-snapshotter/nativeconverter/estargz"
+	zstdchunkedconvert "github.com/containerd/stargz-snapshotter/nativeconverter/zstdchunked"
 	"github.com/goharbor/acceleration-service/pkg/content"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/pkg/errors"
 )
+
+const (
+	DefaultCompressFormat = "estargz"
+	ZstdChunkedFormat     = "zstdchunked"
+)
+
+type layerConverter func([]estargz.Option) converter.ConvertFunc
+
+var compressFormatConverters = map[string]layerConverter{
+	DefaultCompressFormat: func(opts []estargz.Option) converter.ConvertFunc {
+		return estargzconvert.LayerConvertFunc(opts...)
+	},
+	ZstdChunkedFormat: func(opts []estargz.Option) converter.ConvertFunc {
+		return zstdchunkedconvert.LayerConvertFunc(opts...)
+	},
+}
 
 type Driver struct {
 	cfg        map[string]string
@@ -37,15 +55,22 @@ func New(cfg map[string]string, platformMC platforms.MatchComparer) (*Driver, er
 }
 
 func (d *Driver) Convert(ctx context.Context, p content.Provider, ref string) (*ocispec.Descriptor, error) {
-	opts, docker2oci, err := getESGZConvertOpts(d.cfg)
+	opts, docker2oci, compressFormat, err := getConvertOpts(d.cfg)
 	if err != nil {
-		return nil, errors.Wrap(err, "parse estargz conversion options")
+		return nil, errors.Wrap(err, "parse conversion options")
 	}
+
+	layerConv, ok := compressFormatConverters[compressFormat]
+	if !ok {
+		return nil, fmt.Errorf("unsupported compress format: %s", compressFormat)
+	}
+
 	image, err := p.Image(ctx, ref)
 	if err != nil {
 		return nil, errors.Wrap(err, "get source image")
 	}
-	return converter.DefaultIndexConvertFunc(estargzconvert.LayerConvertFunc(opts...), docker2oci, d.platformMC)(
+
+	return converter.DefaultIndexConvertFunc(layerConv(opts), docker2oci, d.platformMC)(
 		ctx, p.ContentStore(), *image)
 }
 
@@ -57,11 +82,16 @@ func (d *Driver) Version() string {
 	return ""
 }
 
-func getESGZConvertOpts(cfg map[string]string) (opts []estargz.Option, docker2oci bool, err error) {
+func getConvertOpts(cfg map[string]string) (opts []estargz.Option, docker2oci bool, compressFormat string, err error) {
+	compressFormat = DefaultCompressFormat
+	if format, ok := cfg["compression"]; ok && format != "" {
+		compressFormat = format
+	}
+
 	if s, ok := cfg["docker2oci"]; ok {
 		b, err := strconv.ParseBool(s)
 		if err != nil {
-			return nil, false, err
+			return nil, false, "", err
 		}
 		docker2oci = b
 	}
